06728904b1c532febbd46c9ec44935f3a1513b89
Varun Balan G <gjvarun0307@gmail.com>
2026-01-30 11:49:09 -0600

    ConvNext model init

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
+Project in progress...
+
 Steps followed:
 1. As in paper started to code ResNet50 from scratch.
 2. In ResNet paper, the ResNet 50 uses bottleneck block instead of basic block, stem block as the entey preprocessing, also implement shortcut when the channel size differs due to increse in channel size and then finally the classification head.
-3. 
+3. The ConvNexT model has following difference from the ResNet:
+    - **Training Techniques**, the model is trainied similar to how a DeiT or Swin Transformers are trained. Adam optimizers, Mixup, Cutmix and Label Smoothing.
+    -  **Changing the Stage compute ratio**, The standard ResNet50 uses these stage ratios (3, 4, 6, 3) and instead we now use these ratio (3, 3, 9, 3), this aligns with FLOPs with Swin-T
+    - **Changing Stem to "Patchify"**, instead of the stem 7x7, stride 2 the paper suggest using patchify layer which is 4x4 stride 4.
+    - **ResNeXt-ify**, the paper suggest on using the Depthwise convolution which turn the calculation similar to transformers, mixing information only in the specific channel.
+    - **Inverted Bottlenecks**, this is directly from transformers models this change reduces flop nad increses accuracy
+    - **Large Kernel Sizes**, this is from swin-T architecture. Moving the depthwise convolution layer and then increasing the kernel size to 7x7.
+    - **Micro Designs**, these include changing ResNet internal compenents to moderen standards. Like changing ReLU to GeLU, fewer activation functions, Fewer normalitation layers, using LN instaed of BN and downsampling layers.
+4. Following above changes to ResNet is dubbed as ConvNexT as quoted in paper.
---
+++ model.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import math
+
+# We are starting with ResNet 50 model then make changes to it as explained in paper for ConvNexT
+# Changed some class names for ConvNext
+# stem block
+class StemBlock(nn.Module):
+    def __init__(self, in_channels=3, out_channels=64):
+        super().__init__()
+        
+        self.stem = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+    def forward(self, x):
+        x = self.stem(x)
+        return x
+        
+
+# Bottleneck block
+class BottleneckBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsampling=1, expansion=4):
+        super().__init__()
+        self.expanded_channels = out_channels*expansion
+        self.need_shortcut = in_channels != self.expanded_channels
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=downsampling, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, self.expanded_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(self.expanded_channels)
+        )
+        
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(in_channels, self.expanded_channels, kernel_size=1, stride=downsampling, bias=False),
+            nn.BatchNorm2d(self.expanded_channels)
+        ) if self.need_shortcut else nn.Identity()
+
+    def forward(self, x):
+        residual = x
+        if self.need_shortcut: residual = self.shortcut(x)
+        x = self.block(x)
+        x += residual
+        x = nn.ReLU(x)
+        return x
+
+# bottle = BottleneckBlock(32, 64)
+# bottle(torch.ones((1, 32, 10, 10))).shape
+# print(bottle)
+
+# ConvNext Stage
+class ConvNextStage(nn.Module):
+    def __init__(self, in_channels, out_channels, n=1, expansion=4):
+        super().__init__()
+        downsampling = 2 if (in_channels != out_channels) else 1
+        self.stage = nn.Sequential(
+            BottleneckBlock(in_channels, out_channels, downsampling, expansion),
+            *[BottleneckBlock(out_channels*expansion, out_channels, downsampling=1) for _ in range(n-1)]
+        )
+
+    def forward(self, x):
+        x = self.stage(x)
+        return x
+
+# dummy = torch.ones((1, 32, 48, 48))
+
+# bottle = ConvNextStage(64, 128, n=3)
+# # bottle(dummy)
+# print(bottle)
+
+# Classification Head
+class ConvNextClassificationHead(nn.Module):
+    def __init__(self, in_features, num_classes):
+        super().__init__()
+
+        self.head = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+            nn.Linear(in_features, num_classes)
+        )
+    
+    def forward(self, x):
+        x = self.head(x)
+        return x
+
+# ConvNext Model 
+class ConvNext(nn.Module):
+    def __init__(self, in_channels, num_classes, expansion=4, block_sizes=[64, 128, 256, 512], depths=[3, 4, 6, 3]):
+        super().__init__()
+
+        self.in_out = list(zip(block_sizes, block_sizes[1:]))
+
+        self.stem = StemBlock(in_channels, block_sizes[0])
+        self.mainblock = nn.Sequential(
+            ConvNextStage(block_sizes[0], block_sizes[0], depths[0]),
+            *[ConvNextStage(in_channel*expansion, out_channel, depth) for ((in_channel, out_channel), depth) in zip(self.in_out, depths[1:])]
+        )
+        self.head = ConvNextClassificationHead(block_sizes[-1]*expansion, num_classes)
+    
+    def forward(self, x):
+        x = self.stem(x)
+        x = self.mainblock(x)
+        x = self.head(x)
+        return x
+    
